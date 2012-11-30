c16fef7c113985c8cb05c98378bf421fe965bfb0
linaro <linaro@linaro-ubuntu-desktop.(none)>
2012-11-30 15:21:18 +0000

    fixes after testing

--- spi_driver/src/spi.py
+++ spi_driver/src/spi.py
@@ -1,14 +1,26 @@
+#!/usr/bin/python
+
 import time
 import Ox.GPIO as GPIO
 
 class SPI:
-	def __init__(self, miso, mosi, clk):
+	def __init__(self, miso, mosi, clk, reset):
 		# set up GPIO output channel
-		GPIO.setup(self.miso = miso, GPIO.IN)
-		GPIO.setup(self.mosi = mosi, GPIO.OUT)
-		GPIO.setup(self.clk  = clk,  GPIO.OUT)
+		GPIO.setup(miso, GPIO.IN)
+		GPIO.setup(mosi, GPIO.OUT)
+		GPIO.setup(clk,  GPIO.OUT)
+		GPIO.setup(reset,GPIO.OUT)
+
+		self.miso  = miso
+		self.mosi  = mosi
+		self.clk  = clk
+		self.reset  = reset
+
+		GPIO.output(self.reset, GPIO.HIGH)
 		GPIO.output(self.mosi, GPIO.LOW)
-		self.rest_ms = 10./1000
+
+		self.rest_ms = 20./10000
+		time.sleep(0.1)
 
 	def reset(self):
 		GPIO.output(self.reset, GPIO.LOW)
@@ -17,19 +29,17 @@ class SPI:
 		time.sleep(0.05)
 
 	def sendByte(self, byte):
+		r = 0
 		for i in range(0,8):
-			GPIO.output(self.mosi, (byte&(1<<i))>0?GPIO.HIGH:GPIO.LOW)
-			GPIO.output(self.mosi, GPIO.HIGH)
+			v = GPIO.LOW
+			if (byte&(1<<(7-i)))>0: v = GPIO.HIGH
+			GPIO.output(self.mosi, v)
+			GPIO.output(self.clk, GPIO.HIGH)
 			time.sleep(self.rest_ms)
-			GPIO.output(self.mosi, GPIO.LOW)
+			r |= (GPIO.input(self.miso)<<i)
+			GPIO.output(self.clk, GPIO.LOW)
 			time.sleep(self.rest_ms)
+		return r
 
-	def readByte(self, byte):
-		byte = 0
-		for i in range(0,8):
-			GPIO.output(self.mosi, GPIO.HIGH)
-			time.sleep(self.rest_ms)
-			byte |= (GPIO.input(self.miso)<<i)
-			GPIO.output(self.mosi, GPIO.LOW)
-			time.sleep(self.rest_ms)
-		return byte
+	def readByte(self):
+		return self.sendByte(0)
