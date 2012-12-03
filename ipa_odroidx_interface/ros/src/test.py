e056fd951d0204548723a356e9d3c173ee379015
linaro <linaro@linaro-ubuntu-desktop.(none)>
2012-12-03 15:05:06 +0000

    blinking

--- ipa_odroidx_interface/ros/src/test.py
+++ ipa_odroidx_interface/ros/src/test.py
@@ -12,9 +12,14 @@ print "Inputs are ", intf.get_input()
 for i in range(0,4):
 	print "Analog ch. ",i," is ", intf.get_analog(i)
 
-while False:
-	print "Inputs are ", intf.get_input()
-	intf.set_output(0,255)
-	intf.set_output(0,0)
+cnt=0
+d=1
+while True:
+	#print "Inputs are ", intf.get_input()
+	intf.set_output(0,cnt)
+	cnt+=d
+	if cnt>=255: d=-1
+	elif cnt==0: d=1
+	#intf.set_output(0,0)
 intf.set_motor(0,1,127)
 intf.set_motor(1,1,0)
