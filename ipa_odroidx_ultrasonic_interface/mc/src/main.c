052c13ab23f113b1b66efaeee4b77a6e6b84dee9
Ali Shuja Siddiqui <alishuja@gmail.com>
2012-12-06 11:06:51 +0100

    Modified code to use Timer0 and generate listening pulse at the same time.

--- ipa_odroidx_ultrasonic_interface/mc/src/main.c
+++ ipa_odroidx_ultrasonic_interface/mc/src/main.c
@@ -19,41 +19,47 @@ volatile unsigned char PING_STAGE_2=FALSE;
 volatile unsigned char PING_STAGE_3=FALSE;
 
 int main(void){
-	DDRA |= (1 << PA1);
-	PORTA |= (1 << PA1); // Setting Initial value
+	DDRD |= (1 << PD1)| (1 << PD3);
+	PORTD &= (0 << PD1); // Setting Initial value
+	PORTD &= (0 << PD3);
 
-	TIMSK |= (1 << TOIE1); // Enabling timer 1 overflow
+	TCNT0 = 1;
+	TCCR0B |= (1 << CS01);
+
+	TIMSK0 |= (1 << TOIE0); // Enabling timer 1 overflow
 	sei(); // Setting global interrupt
 
-	TCNT1 = 1;
-	TCCR1B |= (1 << CS10);
 
 	for(;;){
 	}
 }
-ISR(TIMER1_OVF_vect){ // Timer 1 is dedicated for Pinging.
+ISR(TIMER0_OVF_vect){ // Timer 0 is dedicated for Pinging and listening.
 	if(PING_STAGE_0 == FALSE){
-		PORTA |= (1 << PA1);
-		TCNT1 = 64775; // Setting for 330us
+		PORTD |= (1 << PD1);
+		PORTD |= (1 << PD3);
+		TCNT0 = 160; // Setting for 330us
 		PING_STAGE_0 = TRUE;
-		TCCR1B &= (0 << CS11);
-		TCCR1B |= (1 << CS10);
+		TCCR0B &= (0 << CS00);
+		TCCR0B &= (0 << CS02);
+		TCCR0B |= (1 << CS01);
 	}
 	else if(PING_STAGE_1 == FALSE){
-		PORTA &= (0 << PA1);
-		TCNT1 = 64729; // Setting for 350us
+		PORTD &= (0 << PD1);
+		TCNT0 = 154; // Setting for 350us -0.02% error
 		PING_STAGE_1 = TRUE;
 	}
 	else if(PING_STAGE_2 == FALSE){
-		PORTA |= (1 << PA1);
-		TCNT1 =  64913; // Setting for 270us
+		PORTD |= (1 << PD1);
+		PORTD &= (0 << PD3);
+		TCNT0 = 177; // Setting for 270us -0.02% error
 		PING_STAGE_2 = TRUE;
 	}
 	else if(PING_STAGE_3 == FALSE){
-		PORTA &= (0 << PA1);
-		TCNT1 = 14126; // Setting for 50m-330u-350u-270u
-		TCCR1B &= (0 << CS10);
-		TCCR1B |= (1 << CS11);
+		PORTD &= (0 << PD1);
+		TCNT0 = 30; // Setting for 100ms
+		TCCR0B &= (0 << CS01);
+		TCCR0B |= (1 << CS00);
+		TCCR0B |= (1 << CS02);
 		PING_STAGE_0 = FALSE;
 		PING_STAGE_1 = FALSE;
 		PING_STAGE_2 = FALSE;
