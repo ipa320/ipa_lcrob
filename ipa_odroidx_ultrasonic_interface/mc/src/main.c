2a95b1354e26c65de86fd37d7eb41a1aa5b0950e
Ali Shuja Siddiqui <josh-as@ipa.fraunhofer.de>
2012-12-11 08:50:09 +0100

    Added code for sending time change data to master using UART.

--- ipa_odroidx_ultrasonic_interface/mc/src/main.c
+++ ipa_odroidx_ultrasonic_interface/mc/src/main.c
@@ -4,7 +4,7 @@
 #include "iocompat.h"
 #include "softuart.h"
 
-#define MAX_NUMBER_OF_VALUES 20
+#define MAX_VALUES 32
 struct TIME_KEEPER{
 	uint8_t port_val;
 	uint16_t time_reg_val;
@@ -19,16 +19,43 @@ struct TIME_KEEPER{
  *whereas MSB is unused.
  */
 
-volatile unsigned char PING_STAGE=0;
+volatile  uint8_t PING_STAGE=0;
 
-volatile unsigned char PORTA_CONTROL=0x7f;
+volatile unsigned char PORTA_CONTROL=0x00;
 volatile unsigned char PORTD_CONTROL=0x33;
 
-volatile struct TIME_KEEPER PORT_B_VALS[MAX_NUMBER_OF_VALUES]; 
-volatile struct TIME_KEEPER PORT_C_VALS[MAX_NUMBER_OF_VALUES];
+volatile struct TIME_KEEPER PC7_VALS[32];
+volatile struct TIME_KEEPER PB6_VALS[32];
+volatile struct TIME_KEEPER PB4_VALS[32];
+volatile struct TIME_KEEPER PB3_VALS[32];
+volatile struct TIME_KEEPER PB2_VALS[32];
+volatile struct TIME_KEEPER PB1_VALS[32];
+volatile struct TIME_KEEPER PB0_VALS[32];
 
-volatile uint8_t COUNT_PORT_B = 0;
-volatile uint8_t COUNT_PORT_C = 0;
+volatile struct TIME_KEEPER PD7_VALS[32];
+volatile struct TIME_KEEPER PC2_VALS[32];
+volatile struct TIME_KEEPER PC3_VALS[32];
+volatile struct TIME_KEEPER PC4_VALS[32];
+volatile struct TIME_KEEPER PC5_VALS[32];
+volatile struct TIME_KEEPER PC6_VALS[32];
+
+
+volatile uint8_t PC7_VALS_count = 0;
+volatile uint8_t PB6_VALS_count = 0;
+volatile uint8_t PB4_VALS_count = 0;
+volatile uint8_t PB3_VALS_count = 0;
+volatile uint8_t PB2_VALS_count = 0;
+volatile uint8_t PB1_VALS_count = 0;
+volatile uint8_t PB0_VALS_count = 0;
+
+volatile uint8_t PD7_VALS_count = 0;
+volatile uint8_t PC2_VALS_count = 0;
+volatile uint8_t PC3_VALS_count = 0;
+volatile uint8_t PC4_VALS_count = 0;
+volatile uint8_t PC5_VALS_count = 0;
+volatile uint8_t PC6_VALS_count = 0;
+
+volatile uint8_t CYCLE_COMPLETE = 0;
 
 int main(void){
 	//Setting PORTA and PORTD as output
@@ -56,7 +83,30 @@ int main(void){
 	sei(); // Setting global interrupt
 
 	for(;;){
-		softuart_putchar('a');
+		if(CYCLE_COMPLETE == 1){
+			for (uint8_t count = 0; count < PC7_VALS_count; count++){
+				softuart_putchar(PC7_VALS[count].port_val);
+				softuart_putchar((uint8_t)((PC7_VALS[count].time_reg_val & 0xFF00)>> 8));
+				softuart_putchar((uint8_t)(PC7_VALS[count].time_reg_val & 0x00FF));
+			}
+			PC7_VALS_count = 0;
+			
+			for (uint8_t count = 0; count < PB6_VALS_count; count++){
+				softuart_putchar(PB6_VALS[count].port_val);
+				softuart_putchar((uint8_t)((PB6_VALS[count].time_reg_val & 0xFF00)>> 8));
+				softuart_putchar((uint8_t)(PB6_VALS[count].time_reg_val & 0x00FF));
+			}
+			PB6_VALS_count = 0;
+
+
+			CYCLE_COMPLETE = 0;
+
+			TCNT0 = 254; // Initializing timer 0 to trigger.
+			TCCR0B |= (1 << CS01);
+		//	TCCR0B |= (1 << CS00);
+			TIFR0 |= (1 << TOV0); //Forced timer0 interrupt trigger.
+			TIMSK0 |= (1 << TOIE0);
+		}
 	}
 }
 ISR(TIMER0_OVF_vect){ // Timer 0 is dedicated for Pinging and listening.
@@ -95,21 +145,50 @@ ISR(TIMER0_OVF_vect){ // Timer 0 is dedicated for Pinging and listening.
 		TCCR1B |= (1 << CS11);
 		TIMSK |= (1 << TOIE1);
 		// Place to enable PCINT
-		//
-		//
-	}
+		// For PA6-PC7
+		if((PORTA_CONTROL & (1<<6))==0x40) {
+			PCMSK2 |= (1 << PCINT23);
+			PCICR |= (1 << PCIE2);
+		}
+		//For PA5-PB6
+		if ((PORTA_CONTROL & (1<<5))==0x20){
+			PCMSK0 |= (1 << PCINT6);
+			PCICR |= (1 << PCIE0);
+		}
+	}	
 }
 ISR(TIMER1_OVF_vect){
 	//Disabling timer1
 	TCCR1B &= ~(1 << CS11);
 	TIMSK &= ~(1 << TOIE1);
 
-	TCNT0 = 255; // Initializing timer 0 to trigger.
-	TCCR0B &= ~(1 << CS01);
-	TCCR0B |= (1 << CS00);
-	TIFR0 |= (1 << TOV0); //Forced timer0 interrupt trigger.
-	TIMSK0 |= (1 << TOIE0);
 	PING_STAGE = 0;
+
+	CYCLE_COMPLETE = 1;
+
+	PCMSK0 = 0x00;
+	PCMSK1 = 0x00;
+	PCMSK2 = 0x00;
+	PCICR &= 0xF0;
+
 
 }
 
+ISR(PCINT2_vect){
+	if((PORTA_CONTROL & (1<<6))==0x40) {
+		if(PC7_VALS_count < MAX_VALUES){
+			PC7_VALS[PC7_VALS_count].port_val=PINC;
+			PC7_VALS[PC7_VALS_count].time_reg_val = TCNT1 - 36735;
+			PC7_VALS_count++;
+		}
+	}
+}
+ISR(PCINT0_vect){
+	if((PORTA_CONTROL & (1<<5))==0x20) {
+		if(PB6_VALS_count < MAX_VALUES){
+			PB6_VALS[PB6_VALS_count].port_val=PINC;
+			PB6_VALS[PB6_VALS_count].time_reg_val = TCNT1 - 36735;
+			PB6_VALS_count++;
+		}
+	}
+}
