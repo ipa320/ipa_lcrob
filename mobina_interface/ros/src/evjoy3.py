4aee8c2cd4d424f19a6c938236bc09378681bdee
Josh <joshua.hampp@ipa.fraunhofer.de>
2013-02-07 03:48:00 -0600

    changed from env to python

---
+++ mobina_interface/ros/src/evjoy3.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python
+import os, time
+from subprocess import Popen, PIPE
+
+if __name__ == "__main__":
+        print "waiting for joystick..."
+	p1 = Popen(["dmesg"], stdout=PIPE)
+	p2 = Popen(["grep", "hid_logitech"], stdin=p1.stdout, stdout=PIPE)
+	while len(p2.communicate())<1:
+		time.sleep(1)
+	os.system("rosrun mobina_interface evjoy2.py")
