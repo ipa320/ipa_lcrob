9df2c9adfca973b216f6aa9bdb93408977fb2e62
Josh <joshua.hampp@ipa.fraunhofer.de>
2013-02-21 06:25:12 -0600

    updated test

--- ipa_odroidx_interface/ros/src/test.py
+++ ipa_odroidx_interface/ros/src/test.py
@@ -13,14 +13,28 @@ print "Inputs are ", intf.get_input()
 for i in range(0,4):
 	print "Analog ch. ",i," is ", intf.get_analog(i)
 
+intf.set_output(0,0)
+intf.set_output(1,0)
+intf.set_output(4,0)
+
+intf.set_output(0, 55)
+intf.set_pulse(0, 1)
+
+intf.set_motoraim0(int(sys.argv[1]))
+exit()
+
 cnt=0
 d=1
-while False:#True:
+while False: #True:
 	#print "Inputs are ", intf.get_input()
 	intf.set_output(0,cnt)
 	cnt+=d
 	if cnt>=255: d=-1
 	elif cnt==0: d=1
 	#intf.set_output(0,0)
-intf.set_motor(0,0,int(sys.argv[1]))
-#intf.set_motor(1,1,0)
+
+v = int(sys.argv[1])
+d=1
+if v<0: d=0
+intf.set_motor(0,d,abs(v))
+intf.set_motor(1,d,abs(v))
